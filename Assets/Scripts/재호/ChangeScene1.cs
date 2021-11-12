f30d8ad24cba14f857ff17f33a173d4362010047
Parion5549 <qtpman0313@gmail.com>
2021-11-13 01:05:48 +0900

    재호존잘
    시작씬, 설명씬

---
+++ Assets/Scripts/재호/ChangeScene1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene1 : MonoBehaviour
+{
+    string nam;
+    void Start()
+    {
+       nam = this.name;
+    }
+
+    public void ChangeSceneBtn()
+    {
+        Debug.Log("버튼누름");
+        if(nam=="StartBtn") SceneManager.LoadScene("Explain");
+    }
+
+    public void ChangeSceneBtn1()
+    {
+        Debug.Log("버튼누름");
+        if (nam == "StartBtn1") SceneManager.LoadScene("InGame");
+    }
+
+    public void QuitGameBtn()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
+
